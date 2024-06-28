79a89aee2945c78d2691055f1ced2ade79e7ccc9
Mr. Fluffy <git@sixearsm>
2024-06-28 17:57:45 +0000

    add cargo

--- components/sshkeys.nix
+++ components/sshkeys.nix
@@ -2,6 +2,18 @@
 
 {
   programs.ssh.knownHosts = {
+    adamson = {
+      hostNames     = [ "adamson" "adamson.sixears.co.uk" "192.168.0.25" ];
+      publicKeyFile = ../sshkeys/adamson.pub;
+    };
+    apparatus = {
+      hostNames     = [ "apparatus" "apparatus.sixears.co.uk" "192.168.0.23" ];
+      publicKeyFile = ../sshkeys/apparatus.pub;
+    };
+    barracuda = {
+      hostNames     = [ "barracuda" "barracuda.sixears.co.uk" "192.168.0.27" ];
+      publicKeyFile = ../sshkeys/barracuda.pub;
+    };
     barry = {
       hostNames     = [ "barry" "barry.sixears.co.uk" "192.168.0.1" ];
       publicKeyFile = ../sshkeys/barry.pub;
@@ -10,30 +22,17 @@
       hostNames     = [ "blues" "blues.sixears.co.uk" "192.168.0.90" ];
       publicKeyFile = ../sshkeys/blues.pub;
     };
-    drifting = {
-      hostNames     = [ "drifting" "drifting.sixears.co.uk" "192.168.0.3" ];
-      publicKeyFile = ../sshkeys/drifting.pub;
+    bukka = {
+      hostNames     = [ "bukka" "bukka.sixears.co.uk" "192.168.0.26" ];
+      publicKeyFile = ../sshkeys/bukka.pub;
     };
-    red = {
-      hostNames     = [ "red" "red.sixears.co.uk" "192.168.0.5" ];
-      publicKeyFile = ../sshkeys/red.pub;
+    bullet = {
+      hostNames     = [ "bullet" "bullet.sixears.co.uk" "192.168.0.21" ];
+      publicKeyFile = ../sshkeys/bullet.pub;
     };
-    dog = {
-      hostNames     = [ "dog"  "dog.sixears.co.uk" "192.168.0.7"
-                        "[sixears.co.uk]:9876" "[82.69.54.62]:9876" ];
-      publicKeyFile = ../sshkeys/dog.pub;
-    };
-    trance = {
-      hostNames     = [ "trance" "trance.sixears.co.uk" "192.168.0.8" ];
-      publicKeyFile = ../sshkeys/trance.pub;
-    };
-   poison = {
-      hostNames     = [ "poison" "poison.sixears.co.uk" "192.168.0.11" ];
-      publicKeyFile = ../sshkeys/poison.pub;
-    };
-    slider = {
-      hostNames     = [ "slider" "slider.sixears.co.uk" "192.168.0.13" ];
-      publicKeyFile = ../sshkeys/slider.pub;
+    cargo = {
+      hostNames     = [ "cargo" "cargo.sixears.co.uk" "192.168.0.10" ];
+      publicKeyFile = ../sshkeys/cargo.pub;
     };
     curse = {
       hostNames     = [ "curse" "curse.sixears.co.uk" "192.168.0.14" ];
@@ -44,13 +43,34 @@
                         "deluge" "deluge.sixears.co.uk" ];
       publicKeyFile = ../sshkeys/defector.pub;
     };
+    dissolve = {
+      hostNames     = [ "dissolve" "dissolve.sixears.co.uk" "192.168.0.28" ];
+      publicKeyFile = ../sshkeys/dissolve.pub;
+    };
+    dog = {
+      hostNames     = [ "dog"  "dog.sixears.co.uk" "192.168.0.7"
+                        "[sixears.co.uk]:9876" "[82.69.54.62]:9876" ];
+      publicKeyFile = ../sshkeys/dog.pub;
+    };
+    drifting = {
+      hostNames     = [ "drifting" "drifting.sixears.co.uk" "192.168.0.3" ];
+      publicKeyFile = ../sshkeys/drifting.pub;
+    };
+    poison = {
+      hostNames     = [ "poison" "poison.sixears.co.uk" "192.168.0.11" ];
+      publicKeyFile = ../sshkeys/poison.pub;
+    };
+    trance = {
+      hostNames     = [ "trance" "trance.sixears.co.uk" "192.168.0.8" ];
+      publicKeyFile = ../sshkeys/trance.pub;
+    };
+    slider = {
+      hostNames     = [ "slider" "slider.sixears.co.uk" "192.168.0.13" ];
+      publicKeyFile = ../sshkeys/slider.pub;
+    };
     stone = {
       hostNames     = [ "stone" "stone.sixears.co.uk" "192.168.0.19" ];
       publicKeyFile = ../sshkeys/stone.pub;
-    };
-    bullet = {
-      hostNames     = [ "bullet" "bullet.sixears.co.uk" "192.168.0.21" ];
-      publicKeyFile = ../sshkeys/bullet.pub;
     };
     york = {
       hostNames     = [ "york" "york.sixears.co.uk" "192.168.0.22" ];
@@ -60,10 +80,6 @@
       hostNames     = [ "slick" "slick.sixears.co.uk" "192.168.0.2" ];
       publicKeyFile = ../sshkeys/slick.pub;
     };
-    apparatus = {
-      hostNames     = [ "apparatus" "apparatus.sixears.co.uk" "192.168.0.23" ];
-      publicKeyFile = ../sshkeys/apparatus.pub;
-    };
     night = {
       hostNames     = [ "night" "night.sixears.co.uk" "192.168.0.24"
                         "[sixears.co.uk]:9875" "[82.69.54.62]:9875"
@@ -71,26 +87,14 @@
                       ];
       publicKeyFile = ../sshkeys/night.pub;
     };
-    adamson = {
-      hostNames     = [ "adamson" "adamson.sixears.co.uk" "192.168.0.25" ];
-      publicKeyFile = ../sshkeys/adamson.pub;
-    };
-    bukka = {
-      hostNames     = [ "bukka" "bukka.sixears.co.uk" "192.168.0.26" ];
-      publicKeyFile = ../sshkeys/bukka.pub;
-    };
-    barracuda = {
-      hostNames     = [ "barracuda" "barracuda.sixears.co.uk" "192.168.0.27" ];
-      publicKeyFile = ../sshkeys/barracuda.pub;
-    };
-    dissolve = {
-      hostNames     = [ "dissolve" "dissolve.sixears.co.uk" "192.168.0.28" ];
-      publicKeyFile = ../sshkeys/dissolve.pub;
-    };
-   panasonic = {
+    panasonic = {
       hostNames     = [ "panasonic" "panasonic.sixears.co.uk" "192.168.0.30"
                         "[panasonic]:2222" "[192.168.0.30]:2222" ];
       publicKeyFile = ../sshkeys/panasonic.pub;
+    };
+    red = {
+      hostNames     = [ "red" "red.sixears.co.uk" "192.168.0.5" ];
+      publicKeyFile = ../sshkeys/red.pub;
     };
     supreme = {
       hostNames     = [ "supreme" "supreme.sixears.co.uk" "192.168.0.71" ];
