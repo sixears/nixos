c98f87b0f87a24e27ce7dc0b9b27f20a4bacd088
Mr. Fluffy <git@sixearsm>
2026-07-17 11:22:41 +0000

    add lidcheck to components/battery-powered.nix

--- components/battery-powered.nix
+++ components/battery-powered.nix
@@ -1,10 +1,22 @@
 { pkgs, ... }:
 
 let
-  lowbat = import ../pkgs/lowbat.nix { inherit pkgs; };
+  lowbat    = import ../pkgs/lowbat.nix { inherit pkgs; };
+  systemctl = "${pkgs.systemd}/bin/systemctl";
+  grep      = "${pkgs.gnugrep}/bin/grep";
+  lid       = "/proc/acpi/button/lid/LID/state";
+  open      = "echo lid open";
+  closed    = "echo lid closed";
+  lidcheck  = "${grep} -iq closed$ ${lid}";
+  logger    = "${pkgs.inetutils}/bin/logger";
+  log       = "${logger} -t lidcheck";
 in
   {
     imports = [ ./fcron.nix ];
 
-    services.fcron.systab = "@ 60s ${lowbat}/bin/lowbat";
+    # see the logs with journalctl -t lidcheck
+    services.fcron.systab = ''
+      @ 60s ${lowbat}/bin/lowbat
+      @ 60s ${lidcheck} && ${closed} && ${systemctl} suspend || ${open} | ${log}
+    '';
   }
